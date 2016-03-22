1a33bc6a3ae026b3e25789067e238d27559391a9
BekzhanKassenov <bekzhan.kassenov@gmail.com>
2016-03-23 00:41:50 +0600

    Added A* algorithm (without testing)

--- app/src/main/java/ShortestPath/Graph.java
+++ app/src/main/java/ShortestPath/Graph.java
@@ -1,17 +1,15 @@
 package ShortestPath;
 
-import org.json.JSONArray;
-import org.json.JSONObject;
-
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.PriorityQueue;
 import java.util.Queue;
+import java.util.Set;
+import java.util.TreeSet;
 import java.util.concurrent.LinkedBlockingQueue;
 
 /**
@@ -27,6 +25,11 @@ import java.util.concurrent.LinkedBlockingQueue;
  *
  */
 public class Graph {
+    /**
+     * Precision of calculations
+     */
+    private static double EPS = 1e-9;
+
     /**
      * Map string label to node
      */
@@ -102,18 +105,18 @@ public class Graph {
     /**
      * SPFA - shortest path finder algorithm
      * Simple BFS-like algorithm for creating
-     * source tree from given node.
+     * source tree from given source.
      *
      * Average complexity is not proven, but it is close
      * to O(N logN).
      *
      * Worst-time complexity is exponential.
      *
-     * @param node is root of source tree
+     * @param source is root of source tree
      * @return Map <Node, Node> where keys are nodes and
      * values are parents of keys in source tree.
      */
-    private Map <Node, Node> runSPFA(Node node) {
+    private Map <Node, Node> runSPFA(Node source) {
         // Distances from root to nodes
         Map <Node, Double> distance = new HashMap <>();
 
@@ -129,10 +132,10 @@ public class Graph {
         Queue <Node> queue = new LinkedBlockingQueue <>();
 
         // Initialize all data structures
-        distance.put(node, 0.0);
-        parent.put(node, null);
-        inQueue.put(node, true);
-        queue.add(node);
+        distance.put(source, 0.0);
+        parent.put(source, null);
+        inQueue.put(source, true);
+        queue.add(source);
 
         while (!queue.isEmpty()) {
             // Take element from queue
@@ -148,7 +151,7 @@ public class Graph {
                 double neighborDistance = distance.get(currentNode) + currentNode.distanceTo(neighbor);
 
                 // If we can update distance
-                if (!distance.containsKey(neighbor) || distance.get(neighbor) > neighborDistance) {
+                if (!distance.containsKey(neighbor) || distance.get(neighbor) - neighborDistance > EPS) {
 
                     // Update distance
                     distance.put(neighbor, neighborDistance);
@@ -161,6 +164,118 @@ public class Graph {
                         inQueue.put(neighbor, true);
                         queue.add(neighbor);
                     }
+                }
+            }
+        }
+
+        return parent;
+    }
+
+    /**
+     * A* algorithm for finding shortest path in a graph
+     * Kind of a improved Dijkstra's algorithm which uses heuristics in
+     * order to sort nodes in priority queue
+     *
+     * @param source first node in the path
+     * @param target last node in the path
+     * @return list of parents of nodes expanded in this algorithm
+     */
+    public Map <Node, Node> runAStar(Node source, final Node target) {
+        // Distance to the node
+        final Map <Node, Double> distance = new HashMap<>();
+
+        // Parent of the node in the source tree
+        final Map <Node, Node> parent = new HashMap<>();
+
+        /**
+         * Class for comparing nodes
+         * Highly dependent on distance to the node and on target
+         */
+        Comparator <Node> comparator = new Comparator<Node>() {
+
+            /**
+             * heuristic(x) = f(x) = g(x) + h(x)
+             * g(x) - current distance to the node
+             * h(x) - approximation of distance from the node to the target
+             * h(x) must be less or equal to the real distance from the node
+             * to the target.
+             *
+             * @param node is the same as x in formulas
+             * @return f(x)
+             */
+            public double heuristic(Node node) {
+                double result = Double.MAX_VALUE;
+                if (distance.containsKey(node)) {
+                    result = distance.get(node) + node.distanceTo(target);
+                }
+
+                return result;
+            }
+
+            /**
+             * Compare nodes by heuristics
+             * @param lhs first node
+             * @param rhs second node
+             * @return -1 if lhs < rhs, 1 if lhs > rhs and 0 if lhs == rhs
+             */
+            @Override
+            public int compare(Node lhs, Node rhs) {
+                double distanceLhs = heuristic(lhs);
+                double distanceRhs = heuristic(rhs);
+
+                if (distanceLhs - distanceRhs < EPS) {
+                    return -1;
+                }
+
+                if (distanceLhs - distanceRhs > EPS) {
+                    return 1;
+                }
+
+                return 0;
+            }
+        };
+
+        // Priority queue for storing expanded nodes
+        TreeSet<Node> priorityQueue = new TreeSet<>(comparator);
+
+        // Initialize algorithm
+        priorityQueue.add(source);
+        distance.put(source, 0.0);
+        parent.put(source, null);
+
+        while (!priorityQueue.isEmpty()) {
+            // Take element from the queue
+            Node currentNode = priorityQueue.pollFirst();
+
+            // Expand it by looking at all neighbors
+            for (Node neighbor: currentNode.getNeighbors()) {
+                double neighborDistance = distance.get(currentNode) + currentNode.distanceTo(neighbor);
+
+                // Try to update distances
+                if (!distance.containsKey(neighbor)) {
+
+                    // Distance must go first, because sorting order
+                    // depends on it
+                    distance.put(neighbor, neighborDistance);
+
+                    // Add to priority queue only after distance update!!!
+                    priorityQueue.add(currentNode);
+
+                    parent.put(neighbor, currentNode);
+
+                } else if (distance.get(neighbor) - neighborDistance > EPS) {
+                    // We must remove node from priority queue because we're gonna change
+                    // distance to it, which will break order of elements in it
+                    priorityQueue.remove(currentNode);
+
+                    // Update the distance before putting to the queue!!!
+                    distance.put(neighbor, neighborDistance);
+
+                    // After updating the distance we can put element to the queue
+                    priorityQueue.add(currentNode);
+
+                    parent.put(neighbor, currentNode);
+
                 }
             }
         }
